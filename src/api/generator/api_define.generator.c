b3f219e0d94bb73e35b28c5179bb610595ac8318
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-11-04 08:29:02 -0300

    att

--- src/api/generator/api_define.generator.c
+++ src/api/generator/api_define.generator.c
@@ -17,10 +17,24 @@ int  private_CAmalgamator_generate_amalgamation(
     long size;
     char *content = (char*)dtw_load_any_content(filename,&size,&is_binary);
     UniversalGarbage_add_simple(garbage, content);
+
+
+    char *sha_content = dtw_generate_sha_from_any(content,size);
+    UniversalGarbage_add_simple(garbage, sha_content);
+
+    bool is_already_included =DtwStringArray_find_position(already_included_sha_list,sha_content) != -1;
+    if(is_already_included){
+
+        UniversalGarbage_free(garbage);
+        return CAMALGAMATOR_OK;
+    }
+
+    DtwStringArray_append(already_included_sha_list, sha_content);
+
     CTextStack *str_file = newCTextStack_string_empty();
     UniversalGarbage_add(garbage,CTextStack_free,str_file);
 
-    int state = PRIVATE_CAMALGAMATOR_COLECTING_CHAR_STATE;
+    int state = PRIVATE_CAMALGAMATOR_NORMAL_STATE;
 
     if(content == NULL || is_binary){
         UniversalGarbage_free(garbage);
@@ -28,65 +42,120 @@ int  private_CAmalgamator_generate_amalgamation(
     }
     for(int i =0; i < size;i++){
 
-
-        bool is_include_point =
-        state == PRIVATE_CAMALGAMATOR_COLECTING_CHAR_STATE &&
-        private_CAmalgamator_is_include_at_point(content,size,i);
-
-        if(is_include_point){
-            state = PRIVATE_CAMALGAMATOR_WATING_FILENAME_STRING_START;
-            continue;
-        }
         char current_char = content[i];
 
-        bool is_multiline_coment_start =
-        state == PRIVATE_CAMALGAMATOR_COLECTING_CHAR_STATE &&
-        private_CAmalgamator_is_start_multiline_coment_at_point(content,size,i);
+        if(state == PRIVATE_CAMALGAMATOR_NORMAL_STATE){
 
-        if(is_multiline_coment_start){
-            state = PRIVATE_CAMALGAMATOR_INSIDE_MULTILINE_COMENT;
-        }
-
-        bool is_multiline_coment_end =
-        state == PRIVATE_CAMALGAMATOR_COLECTING_CHAR_STATE &&
-        private_CAmalgamator_is_end_multiline_coment_at_point(content,size,i);
-
-        bool normal_code =state = PRIVATE_CAMALGAMATOR_COLECTING_CHAR_STATE;
-        if(normal_code){
-            CTextStack_format(final,"%c", current_char);
-            continue;
-        }
-
-
-        bool is_str_char = current_char == '"';
-        bool is_include_string_beguining = state == PRIVATE_CAMALGAMATOR_WATING_FILENAME_STRING_START && is_str_char;
-        if(is_include_string_beguining){
-            state = PRIVATE_CAMALGAMATOR_COLLECTING_FILENAME;
-            continue;
-        }
-
-
-        bool is_inside_include_string = state == PRIVATE_CAMALGAMATOR_COLLECTING_FILENAME && !is_str_char;
-        if(is_inside_include_string){
-            CTextStack_format(str_file,"%c",current_char);
-        }
-
-
-        bool is_include_string_end =state == PRIVATE_CAMALGAMATOR_COLLECTING_FILENAME && is_str_char;
-        if(is_include_string_end){
-            int error = private_CAmalgamator_generate_amalgamation(final,str_file->rendered_text,already_included_sha_list);
-            if(error){
-                UniversalGarbage_free(garbage);
-                return error;
+            bool is_multiline_coment_start = private_CAmalgamator_is_start_multiline_coment_at_point(content,size,i);
+            if(is_multiline_coment_start){
+                state = PRIVATE_CAMALGAMATOR_INSIDE_MULTILINE_COMENT;
+                CTextStack_format(final,"%c",current_char);
+                continue;
             }
-            CTextStack_restart(str_file);
-            state = PRIVATE_CAMALGAMATOR_COLECTING_CHAR_STATE;
+
+            bool is_inline_coment_start =  private_CAmalgamator_is_start_inline_coment_at_point(content,size,i);
+            if(is_inline_coment_start){
+                state = PRIVATE_CAMALGAMATOR_INSIDE_INLINE_COMENT;
+                CTextStack_format(final,"%c",current_char);
+                continue;
+            }
+
+            bool is_str_start = current_char == '"';
+            if(is_str_start){
+                state = PRIVATE_CAMALGAMATOR_INSIDE_NORMAL_STRING;
+                CTextStack_format(final,"%c",current_char);
+                continue;
+            }
+
+            bool is_char_start = current_char == '\'';
+            if(is_char_start){
+                state = PRIVATE_CAMALGAMATOR_INSIDE_CHAR;
+                CTextStack_format(final,"%c",current_char);
+                continue;
+            }
+
+            bool is_include = private_CAmalgamator_is_include_at_point(content,size,i);
+            if(is_include){
+                state =PRIVATE_CAMALGAMATOR_WATING_FILENAME_STRING_START;
+                continue;; // we dont format include here
+            }
+
+            CTextStack_format(final,"%c",current_char);
+            continue;
         }
 
+        if(state == PRIVATE_CAMALGAMATOR_INSIDE_MULTILINE_COMENT ){
+            bool is_multiline_coment_end = private_CAmalgamator_is_end_multiline_coment_at_point(content,size,i);
+            if(is_multiline_coment_end){
+                state = PRIVATE_CAMALGAMATOR_NORMAL_STATE;
+            }
+            CTextStack_format(final,"%c",current_char);
+            continue;
 
+        }
 
-        char current = content[i];
+        if(state == PRIVATE_CAMALGAMATOR_INSIDE_INLINE_COMENT){
+            bool is_inline_comment_end = current_char == '\n';
+            if(is_inline_comment_end){
+                state = PRIVATE_CAMALGAMATOR_NORMAL_STATE;
+            }
+            CTextStack_format(final,"%c",current_char);
+            continue;
+        }
 
+        if(state == PRIVATE_CAMALGAMATOR_INSIDE_NORMAL_STRING){
+            char last_char = content[i-1];
+            bool is_str_end = current_char == '"' && last_char != '\\';
+            if(is_str_end){
+                state = PRIVATE_CAMALGAMATOR_NORMAL_STATE;
+            }
+            CTextStack_format(final,"%c",current_char);
+            continue;
+        }
+
+        if(state == PRIVATE_CAMALGAMATOR_INSIDE_CHAR){
+            char last_char = content[i-1];
+            bool is_char_end = current_char == '\''&& last_char != '\'';
+            if(is_char_end){
+                state = PRIVATE_CAMALGAMATOR_NORMAL_STATE;
+            }
+            CTextStack_format(final,"%c",current_char);
+            continue;
+        }
+
+        if(state == PRIVATE_CAMALGAMATOR_WATING_FILENAME_STRING_START){
+            if (current_char == '"'){
+                state = PRIVATE_CAMALGAMATOR_COLLECTING_FILENAME;
+                continue;
+            }
+            if(current_char == '<'){
+                state = PRIVATE_CAMALGAMATOR_NORMAL_STATE;
+                //aborts inclusion
+                CTextStack_format(final,"#include < ");
+                continue;
+
+            }
+        }
+
+        if(state == PRIVATE_CAMALGAMATOR_COLLECTING_FILENAME){
+
+            if(current_char == '"'){
+
+                int error = private_CAmalgamator_generate_amalgamation(final,str_file->rendered_text,already_included_sha_list);
+                if(error){
+                    UniversalGarbage_free(garbage);
+                    return error;
+                }
+
+                CTextStack_restart(str_file);
+                state = PRIVATE_CAMALGAMATOR_NORMAL_STATE;
+                continue;
+            }
+
+            CTextStack_format(str_file,"%c", current_char);
+            continue;
+        }
+        UniversalGarbage_free(garbage);
     }
 
     return CAMALGAMATOR_OK;
--- src/cli/main.c
+++ src/cli/main.c
@@ -5,5 +5,10 @@
 //silver_chain_scope_end
 
 int main(){
+    CTextStack *final = newCTextStack_string_empty();
+    DtwStringArray *already_included = newDtwStringArray();
+    const char *filnemae = "doTheWorld_test/one.c";
+    private_CAmalgamator_generate_amalgamation(final,filnemae,already_included);
+    dtw_write_string_file_content("saida.c", final->rendered_text);
 
 }
