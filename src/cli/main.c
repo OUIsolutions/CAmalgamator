2420ebd6a0e5ee7ff1176b4e4792408ddd34709a
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-11-04 12:29:08 -0300

    att

--- src/cli/main.c
+++ src/cli/main.c
@@ -8,8 +8,8 @@
 
 
 int main(){
-    CAmalgamator_max_plotage = 20;
-    CAmalgamator_min_plotage = 0;
+    CAmalgamator_max_plotage = -1;
+    CAmalgamator_min_plotage = 3913-10;
     #ifdef CAMALGAMATOR_DEBUG
                 CAmalgamation_append("main");
     #endif
