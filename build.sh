aa1ff169d2a8b66125dfe25b048da4a7aad71164
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-11-06 06:51:32 -0300

    att

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 rm -rf release
 mkdir release
-gcc src/cli/main.c -o release/CAmalgamator.o
-./release/CAmalgamator.o --file   src/cli/main.c -o release/CAmalgamator.c
-./release/CAmalgamator.o --file src/imports/imports.api_define.h  -o release/CAmalgamatorApiOne.h
-./release/CAmalgamator.o --file  src/imports/imports.api_define.h  -o release/CAmalgamatorApiNoDependenciesIncluded.h \
+gcc src/cli/main.c -o CAmalgamator.o
+./CAmalgamator.o --file   src/cli/main.c -o release/CAmalgamator.c
+./CAmalgamator.o --file src/imports/imports.api_define.h  -o release/CAmalgamatorApiOne.h
+./CAmalgamator.o --file  src/imports/imports.api_define.h  -o release/CAmalgamatorApiNoDependenciesIncluded.h \
 --noinclude  dependencies
